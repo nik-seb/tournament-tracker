e9fa750254b9c7a3cf878fb8b98fc059fb92b188
Nikolai Humphrey-Blanco <nikolai.humphreyb@gmail.com>
2022-04-12 16:00:44 -0400

    Add options in tournament details

--- vue/src/views/ViewTournament.vue
+++ vue/src/views/ViewTournament.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
       <tournament-details v-bind:tournamentID="$route.params.id" />
+      <tourn-options />
       <!-- TO-DO: add tournament bracket component and join/edit/invite buttons-->
   </div>
 </template>
 
 <script>
 import TournamentDetails from '../components/TournamentDetails.vue'
+import TournOptions from '../components/TournOptions.vue'
 export default {
-  components: { TournamentDetails },
+  components: { TournamentDetails, TournOptions },
 }
 </script>
 
