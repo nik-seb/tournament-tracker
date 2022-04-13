fb1c9fae747d6bc707b7409c232822fdeb664e3c
Nikolai Humphrey-Blanco <nikolai.humphreyb@gmail.com>
2022-04-13 18:58:02 -0400

    Bug fix

--- vue/src/components/EventContainers.vue
+++ vue/src/components/EventContainers.vue
@@ -1,33 +1,32 @@
 <template>
 
-    <body>
+  <div id="ev-container-body">
     <div>
       <!-- title and pic should be moved to Home view; will need to move related CSS too -->
-       <h1 id="company-name"> RANNS Tournament Displays™ </h1>
-         <div class="tournament-picture">
+        <h1 id="company-name"> RANNS Tournament Displays™ </h1>
+        <div class="tournament-picture">
               <img src="../assets/tournament_picture.jpg" alt="example of tournament picture">         
-         </div> 
-          <div class="events">
-            <div id="past">
-          <!-- create a v-if condition if date is less than end date display -->
-          <p id="past-events"> Past Events </p>
-            <event-table class="prior-events" v-bind:filteredList="tournamentArray"/>
-            </div>
-          <!-- create a v-if condition if date is between start date and end date display -->
-<div id="current">
-          <p id="live-events"> Current Events </p>
-            <event-table class="live-events" v-bind:filteredList="tournamentArray"/>
-</div>
-<div id="future">
-          <!-- create a v-if condition if date is greater than end date display -->
-          <div>
-          <p id="upcoming-events"> Upcoming Events </p>
-          <event-table class="upcoming-events" v-bind:filteredList="tournamentArray"/>  
-</div>
-   </div>   
+        </div> 
+        <div class="events">
+          <div id="past">
+            <!-- create a v-if condition if date is less than end date display -->
+            <p id="past-events"> Past Events </p>
+              <event-table class="prior-events" v-bind:filteredList="tournamentArray"/>
           </div>
+            <!-- create a v-if condition if date is between start date and end date display -->
+          <div id="current">
+            <p id="live-events"> Current Events </p>
+              <event-table class="live-events" v-bind:filteredList="tournamentArray"/>
+          </div>  
+          <div id="future">
+            <!-- create a v-if condition if date is greater than end date display -->
+            <p id="upcoming-events"> Upcoming Events </p>
+            <event-table class="upcoming-events" v-bind:filteredList="tournamentArray"/>  
+          </div>
+      </div>   
+    </div>
 
- </body>
+  </div>
 </template>
 
 
@@ -73,18 +72,7 @@ export default {
 .events{
   display: flex;
   flex-direction: row;
-<<<<<<< HEAD
   margin-left: 13%;
-=======
-  justify-content: space-evenly;
-  font-family: Georgia, 'Times New Roman', Times, serif;
-  font-weight: 55%;
-  font-size: 20px;
-  padding-top: 1%;
-  /* align-items: center; */
-  /* margin: 20px */
-  
->>>>>>> 4799b333631cb2c5e57377e00b79a8ae143c4c54
 }
 
 #live-events{
@@ -95,11 +83,8 @@ export default {
   text-decoration-line: underline;
   text-decoration-style: double;
   text-decoration-skip-ink: none;
-<<<<<<< HEAD
   font-size: 175%;
   
-=======
->>>>>>> 4799b333631cb2c5e57377e00b79a8ae143c4c54
 }
 
 #upcoming-events{
