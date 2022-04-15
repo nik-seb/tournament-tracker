6f515c28a4b116034825c371c9b40cd855b9b92d
Nikolai Humphrey-Blanco <nikolai.humphreyb@gmail.com>
2022-04-14 20:43:27 -0400

    Minor rearrangements and prep for bracket and tournament browsing

--- vue/src/components/Bracket.vue
+++ vue/src/components/Bracket.vue
@@ -11,6 +11,59 @@ export default {
     name: 'bracket',
     props: {
         tournamentID: Number
+    },
+    data () {
+        return {
+            tournament: {
+                name: 'Cool tournament',
+                startDate: '1990-04-05',
+                endDate: '1999-08-09',
+                sportId: '5',
+                numOfTeams: '99',   
+                id: 1
+            },
+            teams: [
+                {
+                    teamId: 1,
+                    teamName: 'Owls'
+                },
+                {
+                    teamId: 2,
+                    teamName: 'Eagles'
+                },
+                {
+                    teamId: 3,
+                    teamName: 'Falcons'
+                },
+                {
+                    teamId: 4,
+                    teamName: 'Larks'
+                },
+                {
+                    teamId: 5,
+                    teamName: 'Toucans'
+                },
+                {
+                    teamId: 6,
+                    teamName: 'Finches'
+                },
+                {
+                    teamId: 7,
+                    teamName: 'Blackbirds'
+                }
+            ],
+            matches: [
+                {
+                    tournamentId: 1,
+                    startDate: '',
+                    startTime: '',
+                    homeTeamId: '',
+                    awayTeamId: '',
+                    locationId: '',
+                    winningTeamId: null
+                }
+            ]
+        }
     }
 }
 </script>
