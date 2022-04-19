e3f7a51f4c08c62e057b4ecc9752a86fc58688f8
Nikolai Humphrey-Blanco <nikolai.humphreyb@gmail.com>
2022-04-19 14:15:46 -0400

    Draft stuff for next part of brackets; added sort to get matches by tournament

--- java/src/main/java/com/techelevator/dao/JdbcMatchesDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcMatchesDao.java
@@ -67,8 +67,23 @@ public class JdbcMatchesDao implements MatchesDao {
         String sql = "SELECT match_id, tournament_id, start_date, start_time, home_team_id, away_team_id, location_id, " +
                 "winning_team_id, round_number" +
                 " FROM matches " +
-                " WHERE tournament_id = ?; ";
+                " WHERE tournament_id = ? " +
+                " ORDER BY match_id ASC";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, tournamentId);
+        while (results.next()) {
+            matches.add(mapRowToMatches(results));
+        }
+        return matches;
+    }
+
+    @Override
+    public List<Matches> getMatchByTournamentAndRound(int tournamentId, int roundNumber) {
+        List<Matches> matches = new ArrayList<>();
+        String sql = "SELECT match_id, tournament_id, start_date, start_time, home_team_id, away_team_id, location_id, " +
+                "winning_team_id, round_number" +
+                " FROM matches " +
+                " WHERE tournament_id = ? AND round_number = ?; ";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, tournamentId, roundNumber);
         while (results.next()) {
             matches.add(mapRowToMatches(results));
         }
